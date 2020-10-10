a0d7173e54878b9e9f0f59094672c9c3b8161916
Vivek Kumar <vivek.kumar@Vivek-Kumar-MBP.local>
2020-10-10 17:51:55 +0530

    well tested & final implementation of json explorer with option to print pretty version

--- src/js/views/TreeViewer/TreeViewer.jsx
+++ src/js/views/TreeViewer/TreeViewer.jsx
@@ -1,12 +1,17 @@
-import { cloneDeep, isObject, isArray } from 'lodash';
 import React, { useState } from 'react';
+import { cloneDeep, isObject, isArray, isString, isNumber, isNil, isBoolean } from 'lodash';
+import { MdArrowDropDown, MdPlayArrow,  } from 'react-icons/md';
+import { CgCopy  } from 'react-icons/cg';
+import { Card } from 'views/generic/card';
+import { Button } from '../generic/button';
 
 export const TreeViewer = ( props ) => {
     const { parsedObject }  = props;
     
     const [ presentation, setPresentation ] = useState( [ {
-        name: 'object',
+        name: getTargetType( parsedObject ),
         position: '',
+        type: getTargetType( parsedObject ),
         children: Object.keys( parsedObject ),
         valueExpanded: false,
         value: parsedObject
@@ -30,12 +35,14 @@ export const TreeViewer = ( props ) => {
         } else {
             target.valueExpanded = true;
             target.backup = target.value;
+            target.type = getTargetType( target.value );
             target.value = Object.keys( target.value ).map( ( key, index ) => {
                 return {
                     name: key,
                     position: '' !== position ? position + '-' + index : `${index}`,
                     children: isObject( target.value[ key ] ) ? Object.keys( target.value[ key ] ) : [],
                     valueExpanded: false,
+                    type: getTargetType( target.value[ key ] ),
                     value: target.value[ key ]
                 };
             } );
@@ -45,33 +52,93 @@ export const TreeViewer = ( props ) => {
     };
 
     return (
-        <div className='view-tree-viewer'>
+        <Card className={'view-tree-viewer'}>
             { renderValue( presentation, expandPresentation )}
-        </div>
+            { renderCopyFormatedButton( parsedObject )}
+        </Card>
     );
 };
 
 //////////////////////////////////////// HELPER FUNCTIONS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
+const renderCopyFormatedButton = ( parsedObject ) => {
+    return <Button
+        label={'Print Formatted JSON in Console'}
+        icon={CgCopy}
+        onClick={ () => {
+            const val = JSON.stringify( parsedObject, null, 4 ) ;
+            console.log( val );
+        }}
+        className={'view-tree-viewer__copy'}
+    />;
+};
+
+const getTargetType = ( target ) => {
+    if( isString( target ) ) {return 'string';}
+    if( isNumber( target ) ) {return 'number';}
+    if( isBoolean( target ) ) {return 'bool';}
+    if ( isNil( target ) ) {return 'nil';}
+    if( isArray( target ) ) {return 'array';}
+    return 'object';
+};
+
+const printTypeComponent = ( type ) => {
+    return (
+        <span className={'view-tree-viewer__subtree__value__type view-tree-viewer__subtree__value__type--' + type } >
+            {type}
+        </span>
+    );
+};
+
 const renderValue = ( presentation, expandPresentation ) => {
 
     return (
         presentation &&
-        <div className='view-tree-viewer__value'>
+        <div className='view-tree-viewer__subtree'>
             {
                 presentation.map( ( node ) => {
                     const canExpand = 0 !== node.children.length;
 
                     return (
                         <>
-                            <span onClick={() => canExpand && expandPresentation( node.position )}>
-                                { `${node.name} ---> ${node.children.length || node.value}` }
-                            </span>
+                        <div className='view-tree-viewer__subtree__value'>
+                            {
+                                node.valueExpanded && canExpand &&
+                                <MdArrowDropDown onClick={ () => expandPresentation( node.position )} size={30} />
+                            }
 
                             {
-                                node.valueExpanded &&
-                                renderValue( node.value, expandPresentation )
+                                !node.valueExpanded && canExpand &&
+                                <MdPlayArrow onClick={ () => expandPresentation( node.position )} />
                             }
+                            {
+                                canExpand ?
+                                <span onClick={() => expandPresentation( node.position )}>
+                                    <span>
+                                        { node.name }
+                                    </span>
+
+                                    { printTypeComponent( node.type ) }
+
+                                    <span className='view-tree-viewer__subtree__value__count'>
+                                        { node.children.length }
+                                    </span>
+                                </span>
+                                :
+                                <span>
+                                    {`${node.name} : ${JSON.stringify( node.value )}` }
+                                    { printTypeComponent( node.type ) }
+                                </span>
+                            }
+                        </div>
+                        {
+                            node.valueExpanded &&
+                            renderValue( node.value, expandPresentation )
+                        }
+
+                        {
+                            
+                        }
                         </>
                     );
                 } )
